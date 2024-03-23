39f1574132959820187e5550f39c75f8e7e8eaff
chimekinglsey <onlinekingsley@gmail.com>
2024-03-23 19:50:00 +0100

    updated lambda

--- python/lambda/get_update_count.py
+++ python/lambda/get_update_count.py
@@ -6,21 +6,19 @@ def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('resume_visitors')
 
-    if event['httpMethod'] == 'GET':
-        # Retrieve the item from DynamoDB
-        response = table.get_item(Key={'id': '0'})
-        
-        # Increment the views_count attribute
-        views_count = response.get('Item', {}).get('views_count', 0) + 1
-        
-        # Update the item in DynamoDB
-        table.update_item(
-            Key={'id': '0'},  # Ensure you're updating the correct item
-            UpdateExpression='SET views_count = :new_count',
-            ExpressionAttributeValues={':new_count': views_count}
-        )
-        
-        return {
-            'statusCode': 200,
-            'body': json.dumps({'Total Views': int(views_count)})
-        }
+    # Retrieve the item from DynamoDB
+    response = table.get_item(Key={'id': '0'})
+    
+    # Increment the views_count attribute
+    views_count = response.get('Item', {}).get('views_count', 0) + 1
+    
+    # Update the item in DynamoDB
+    table.update_item(
+        Key={'id': '0'},  # Ensure you're updating the correct item
+        UpdateExpression='SET views_count = :new_count',
+        ExpressionAttributeValues={':new_count': views_count}
+    )
+    
+    return {
+        'views': int(views_count)
+    }
